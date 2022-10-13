d7b9e53d0b055d8e1d52cd4a6736b6aed13fc3e3
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2022-10-13 19:53:06 -0300

    tests: cria Card.test e ajusta ReadMore
    O componente ReadMore estava renderizando <span> extra

--- src/__tests__/Card.test.tsx
+++ src/__tests__/Card.test.tsx
@@ -1,8 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import { Card } from '../components/Card'
 
-test( 'teste', () => {
+test('it must have an image and a title', async () => {
+  const item = {
+    "otherNames": [],
+    "name": "Ein",
+    "description": "Ein is a Pembroke Welsh Corgi brought aboard the Bebop by Spike after a failed attempt to capture a bounty.",
+    "image": { "original": "" }
+  }
 
-  expect(true).toBe(true)
+  render(<Card item={item} />)
 
-} )
+  const image = await screen.findByRole('img')
+  expect(image).toBeInTheDocument()
 
-export {}; 
+  const title = await screen.findByRole('heading')
+  expect(title).toBeInTheDocument()
+})
+
+test('show "Leia Mais" button when description text is bigger than 110 caracters', async () => {
+  const item = {
+    "otherNames": [],
+    "name": "Ein",
+    "description": "The self-invented personal name of an elite net-diver from Earth, hacker Radical Edward is a very strange, somewhat androgynous, teenage girl assumed to be around",
+    "image": { "original": "" }
+  }
+
+  render(<Card item={item} />)
+  const readMore = await screen.findByRole('button')
+  expect(readMore).toHaveTextContent('...Leia mais')
+})
+
+export { }; 
--- src/components/ReadMore.tsx
+++ src/components/ReadMore.tsx
@@ -2,33 +2,36 @@ import { useState } from "react"
 import StyledReadMore from "./styles/ReadMore.styled"
 
 type ReadMoreProps = {
-    children: string
-    limit: number
+   children: string
+   limit: number
 }
 
 export function ReadMore({ children, limit }: ReadMoreProps) {
-    const text = children
-    const [isReadMore, setIsReadMore] = useState(true)
-    const toggleReadMore = () => {
-        setIsReadMore(!isReadMore)
-    }
+   const text = children
+   const [isReadMore, setIsReadMore] = useState(true)
+   const toggleReadMore = () => {
+      setIsReadMore(!isReadMore)
+   }
 
-    const isTextBiggerThan = (limit: number) => {
-        return text.length > limit
-    }
+   const isTextBiggerThan = (limit: number) => {
+      return text.length > limit
+   }
 
-    return (
-        <>
-            <p>
-                {(isReadMore && isTextBiggerThan(limit)) ? text.slice(0, limit) : text}
-                <StyledReadMore>
-                    <span onClick={toggleReadMore} >
-                        {(isReadMore && isTextBiggerThan(limit)) ? "...Leia mais" : " "}
-                        {(!isReadMore && isTextBiggerThan(limit)) ? "Mostrar Menos" : ""}
-                    </span>
-                </StyledReadMore>
-            </p>
-        </>
-    )
+   return (
+      <>
+         <p>
+            {(isReadMore && isTextBiggerThan(limit)) ? text.slice(0, limit) : text}
+            {
+               isTextBiggerThan(limit) &&
+               <StyledReadMore>
+                  <span onClick={toggleReadMore} role="button">
+                     {(isReadMore && isTextBiggerThan(limit)) ? "...Leia mais" : " "}
+                     {(!isReadMore && isTextBiggerThan(limit)) ? "Mostrar Menos" : ""}
+                  </span>
+               </StyledReadMore>
+            }
+         </p>
+      </>
+   )
 
 }
