5fc52ee70cbb6d9d6d3d4da46eba086c3d59aca8
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2022-10-14 01:17:25 -0300

    feat(storybook): cria Card.stories

---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Card } from '../components/Card';
+
+export default {
+  title: 'AnimaVita/Card',
+  component: Card,
+  argTypes: {
+    item:{
+      description: 'Objeto com as propriedades otherNames, name, description e image.'
+    }
+  },
+} as ComponentMeta<typeof Card>;
+
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  item: {
+    "otherNames": ["Running Rock", "Black Dog"],
+    "name": "Jet Black",
+    "description": `Jet, known on his home satellite as the "Black Dog" for his tenacity, 
+      is a 36-year-old former cop from Ganymede (a Jovian satellite) and acts as Spike's foil during the series.`,
+    "image": { "original": "https://media.kitsu.io/characters/images/1/original.jpg" }
+  }
+};
